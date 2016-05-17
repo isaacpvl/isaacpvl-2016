4e2b7c40b3ff71cc63fc24de6ffce6912346535a
Isaac Paavola <isaac@isaacpvl.com>
2016-05-16 22:05:26 -0500

    i know how to javascript

--- public/_scripts/loaderp.js
+++ public/_scripts/loaderp.js
@@ -3,9 +3,8 @@ $(window).load(function() {
 	setTimeout(function() {
 		$('#loader').addClass('out');
 	}, 500+750);
-
 	setTimeout(function() {
 		$('#loader').fadeOut('fast');
 	}, 1750+750);
 
-})
+});
--- scripts/loader.js
+++ scripts/loader.js
@@ -3,9 +3,8 @@ $(window).load(function() {
 	setTimeout(function() {
 		$('#loader').addClass('out');
 	}, 750+1000);
-
 	setTimeout(function() {
 		$('#loader').fadeOut('fast');
 	}, 3000+1000);
 
-})
+});
